6b84faa6f36b82f9fd4075a43fa2b7d01c2587dc
saratambal-20 <sara37873@gmail.com>
2020-09-12 20:46:25 +0300

    lesson 18

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,7 +7,48 @@ $(function () {
     $(this).toggleClass("fa-angle-right fa-angle-down");
     $(this).parent("a").next(".child-links").slideToggle();
   });
+  $(".toggle-fullscreen").on("click", function () {
+    $(this).toggleClass("full-screen");
+    if ($(this).hasClass("full-screen")) {
+      openFullscreen();
+    } else {
+      closeFullscreen();
+    }
+  });
 });
+var elem = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) {
+    /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) {
+    /* Chrome, Safari and Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    /* Firefox */
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    /* Chrome, Safari and Opera */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    /* IE/Edge */
+    document.msExitFullscreen();
+  }
+}
 
 const myMap = new Map();
 
